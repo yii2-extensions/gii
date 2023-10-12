efde024b13680a5d8e058fecabe197e7bba476e9
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2023-10-12 08:38:50 -0300

    Add `config` to `yiisoft/config`. (#8)

---
+++ config/params.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    // gii
+    'yii.gii' => false, // enable gii mode
+    'yii.gii.allowedIPs' => ['127.0.0.1', '::1'], // allow gii on localhost only
+];
